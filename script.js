76652160292af5a38815cc187341cae634653280
or404 <oryak10@gmail.com>
2023-10-01 16:27:23 +0300

    organize code and parentheses feature progress

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const buttons = document.querySelectorAll('button:not(#equals)');
+const numsAndOperatorsButton = document.querySelectorAll('button:not(#equals):not(#clear):not(#DEL):not(.parentheses)');
+const parentheses = document.querySelectorAll('.parentheses');
 const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#DEL');
 const display = document.querySelector('.display');
 const calculationArr = [];
 
@@ -22,13 +25,36 @@ time.textContent = `${hours}:${minutes}`;
 updateClock();
 setInterval(updateClock,30000);
 
-    buttons.forEach(button=>{
+
+function orderOfOperations (calculationArr) {
+    if(typeof calculationArr[calculationArr.length-1] === 'number') {
+        while(calculationArr.includes('*') ||calculationArr.includes('/') ){
+        for(let i = 0 ;  i<calculationArr.length ; i++) {
+            let currentCalculation = null;
+            if(calculationArr[i] === '*' || calculationArr[i] === '/'){
+                currentCalculation = operate(calculationArr[i-1],calculationArr[i],calculationArr[i+1]);
+                calculationArr.splice(i-1,3,currentCalculation);
+            }}}
+        while(calculationArr.includes('+') || calculationArr.includes('-')){
+        for(let i = 0 ; i<calculationArr.length ; i++){
+            let currentCalculation2 = null;
+            if(calculationArr[i] === '+' || calculationArr[i] === '-'){
+            currentCalculation2 = operate(calculationArr[i-1],calculationArr[i],calculationArr[i+1]);
+            calculationArr.splice(i-1,3,currentCalculation2);
+           console.log(currentCalculation2);
+        }}}
+        display.textContent = calculationArr.join(''); 
+    }
+}
+
+
+    numsAndOperatorsButton.forEach(button=>{
         button.addEventListener('click' , ()=>{
             let lastElement = calculationArr[calculationArr.length-1];
             if(typeof lastElement === 'undefined') {
                 if(button.classList.contains('number')){calculationArr.push(parseFloat(button.textContent))};
             }
-            else if(button.classList.contains('operator') && typeof lastElement === 'number' && button.id !== 'dot') {
+            else if(button.classList.contains('operator')  && typeof lastElement === 'number' && button.id !== 'dot') {
                 calculationArr.push(button.textContent);  
             }
             else if(button.classList.contains('number')) {
@@ -39,45 +65,53 @@ setInterval(updateClock,30000);
             else if(button.id === 'dot' && !lastElement.toString().includes('.')  ){
                 if(typeof lastElement === 'number'){calculationArr[calculationArr.length - 1] +='.'};
             }
-            else if(button.id === 'clear') {calculationArr.length = 0;}
-            else if(button.id === 'DEL'){calculationArr.pop();}
-
             display.textContent = calculationArr.join('');
             console.log(calculationArr);
             buttonClickEffect(button);
             });
         });
-
-
+        
+        clearButton.addEventListener('click' , ()=>{
+            calculationArr.length = 0;
+            display.textContent = calculationArr.join('');
+            buttonClickEffect(clearButton);
+        })
+        deleteButton.addEventListener('click' , ()=>{
+            calculationArr.pop();
+            display.textContent = calculationArr.join('');
+            buttonClickEffect(deleteButton);
+        })
         equalsButton.addEventListener('click' , ()=>{
-
-            if(typeof calculationArr[calculationArr.length-1] === 'number') {
-            while(calculationArr.includes('*') ||calculationArr.includes('/') ){
-            for(let i = 0 ;  i<calculationArr.length ; i++) {
-                let currentCalculation = null;
-                if(calculationArr[i] === '*' || calculationArr[i] === '/'){
-                    currentCalculation = operate(calculationArr[i-1],calculationArr[i],calculationArr[i+1]);
-                    calculationArr.splice(i-1,3,currentCalculation);
-                }}}
-            
-            
-            while(calculationArr.includes('+') || calculationArr.includes('-')){
-            for(let i = 0 ; i<calculationArr.length ; i++){
-                let currentCalculation2 = null;
-                if(calculationArr[i] === '+' || calculationArr[i] === '-'){
-                currentCalculation2 = operate(calculationArr[i-1],calculationArr[i],calculationArr[i+1]);
-                calculationArr.splice(i-1,3,currentCalculation2);
-               console.log(currentCalculation2);
-            }}}
-            display.textContent = calculationArr.join(''); 
-        }
             buttonClickEffect(equalsButton);
+            orderOfOperations(calculationArr);
         })
 
-   
-    
-            
-        
+
+
+
+        let toggleParentheses = false;
+        parentheses.forEach(button=>{
+            button.addEventListener('click' , ()=>{
+            let lastElement = calculationArr[calculationArr.length-1];
+
+                if(button.id === 'left-parentheses'&& typeof lastElement === 'string' && operatorInArrayCheck(calculationArr)) {
+                    buttonClickEffect(button);
+                }
+
+
+
+
+
+
+
+
+
+
+
+                //buttonClickEffect(button);
+            })
+        })
+      
 
 
 
@@ -94,12 +128,11 @@ setInterval(updateClock,30000);
 
 
 
-
-
-
-
-
-
+function operatorInArrayCheck(array) {
+   if(array.includes('/') || array.includes('*') || array.includes('-') || array.includes('+')){
+    return true;
+   }
+}
 
 function add(a,b) {
     return a + b;
